0048b027462698dc14d01419e736dde2ec4dd812
Malaki Vandas <malakivandas@gmail.com>
2023-01-04 15:32:14 +0100

    Changed from using API to a node package

---
+++ index.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+const path = require('path');
+
+const argv = require('yargs/yargs')(process.argv.slice(2))
+  .usage('Usage: [-f | --file <path>] [-t | --tense <string>]')
+  .option('f', {
+    alias: 'file',
+    demandOption: true,
+    describe:
+      'The path to the list of verbs to conjugate (.json)',
+    type: 'string',
+  })
+  .option('t', {
+    alias: 'tense',
+    demandOption: true,
+    describe: 'The verb tense(s) selected for conjugation',
+    type: 'array',
+    choices: [
+      'present',
+      'futur',
+      'imparfait',
+      'passe_simple',
+      'conditionnel_present',
+      'imperatif_present',
+      'subjonctif_present',
+      'subjonctif_imparfait',
+      'passe_compose',
+      'plus_que_parfait',
+    ],
+  })
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .example('$0 -f ./verbs-list.json -t tense')
+  .example('$0 -f ./verbs-list.json -t tense1 tense2').argv;
+
+// console.log(path.join(__dirname, argv.file));
+
+const verbs = require(path.join(__dirname, argv.file)).verbs;
+
+argv.tense.map((tense) => {
+  verbs.map((verb) => {
+    console.log(verb, tense);
+  });
+});
---
+++ utilities/verbConjugator.js
@@ -0,0 +1,6 @@
+const FrenchVerbs = require('french-verbs');
+const verbsData = require('../verbs.json');
+
+verb_conjugator;
+
+verbsData.verbs.map(async (verb) => {});
